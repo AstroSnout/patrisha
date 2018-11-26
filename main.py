e5a6bc394d18319fc676ba1939d52c62e2161ca8
AstroSnout <tasin.filip@gmail.com>
2018-11-27 00:08:29 +0100

    Various changes

--- main.py
+++ main.py
@@ -4,145 +4,129 @@ from discord.ext import commands
 import sys
 import traceback
 import asyncio
-import time
 import datetime
-import youtube_dl
+import cogs
 
 from helpers import cfg, bg_tasks, misc
 from helpers.db_manager import DBManager
 
-"""This is a multi file example showcasing many features of the command extension and the use of cogs.
-These are examples only and are not intended to be used as a fully functioning bot. Rather they should give you a basic
-understanding and platform for creating your own bot.
-These examples make use of Python 3.6.2 and the rewrite version on the lib.
-For examples on cogs for the async version:
-https://gist.github.com/leovoel/46cd89ed6a8f41fd09c5
-Rewrite Documentation:
-http://discordpy.readthedocs.io/en/rewrite/api.html
-Rewrite Commands Documentation:
-http://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html
-Familiarising yourself with the documentation will greatly help you in creating your bot and using cogs.
-"""
+class Patrisha(discord.ext.commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bg_task_list = [
+            bg_tasks.guild_sub_sending,
+            bg_tasks.new_atb_apps
+        ]
+        self.bg_task = self.loop.create_task(
+            self.bg_tasks()
+        )
+        self.version = '2.5.1'
+        self.dbm = DBManager()
+
+    async def on_ready(self):
+        await self.change_presence(activity=discord.Game(name='Evolving \o/'))
+
+        print(f'Logged in as: {self.user.name} - {self.user.id}')
+        print(f'Version: {self.version}')
+        print('---------------------')
+
+        for guild in self.guilds:
+            await self.dbm.init_settings(guild)
+
+        print('---------------------')
+        print(f'Successfully logged in and booted in {len(self.guilds)} servers...!')
+        print('---------------------')
+    
+    async def on_message(self, message):
+        await self.process_commands(message)
+
+
+    async def on_guild_join(self, guild):
+        await self.dbm.init_settings(guild)
+
+
+    async def on_member_join(self, member):
+        server_message = await self.dbm.get_on_join_message(member.guild)
+        await member.send(server_message)
+
+        member_role = await self.dbm.get_on_join_role(member.guild)
+        for role in member.guild.roles:
+            if member_role.lower() == role.name.lower():
+                await member.add_roles(role, reason='Default role by PatrishaBot')
+
+
+    async def on_command_error(self, ctx, error):
+        print(ctx, error)
+        error_channel = self.get_channel(504693518023917568)
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+        await error_channel.send(
+            f'[{datetime.datetime.now():%H:%M:%S}] User {ctx.author.name} sent `{ctx.message.content}` that caused an error:'
+            f'\n```{error}```'
+        )
+    
+        if 'Signature extraction failed' in str(error):
+            await ctx.send('YouTube probably pushed an update that broke something :(')
+        if 'ffmpeg was not found' in str(error):
+            await ctx.send(f'My magnificent creator forgot to give me ffmpeg :)')
+        if isinstance(error, discord.errors.Forbidden):
+            await ctx.send(f'I am missing the required permissions to do this command')
+        if isinstance(error, commands.errors.CommandOnCooldown):
+            # time_str = time.strftime("%H:%M:%S", time.gmtime(int(error.retry_after)))
+            await ctx.send(f'You are on cooldown. Try again in {str(error.retry_after)[:4]}s')
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.send(f':x: I need more information in order to do that command!\n'
+                           f'Try running `!help <command name>`')
+
+    async def bg_tasks(self):
+        try:
+            await self.wait_until_ready()
+
+            bg_tasks_channel = self.get_channel(504697387927732234)
+
+            print('The bot is now ready!')
+            print('---------------------')
+            while not self.is_closed():
+                for task in self.bg_task_list:
+                    await task(self)
+                    print(f'Executed BG task: {task.__name__}')
+                    await bg_tasks_channel.send(f'```[{datetime.datetime.now():%H:%M:%S}] Executed BG task: {task.__name__}```')
+                await asyncio.sleep(120)  # task runs every 1200 seconds
+        except Exception as e:
+            print('Error!')
+            print(traceback.print_exc())
+            self.bg_task = self.loop.create_task(
+                self.bg_tasks()
+            )
+
 # Bot wants ManageMessages, ReadChannelHistory
-
-
 def get_prefix(bot, message):
     prefixes = ['!']
     if not message.guild:
         return '!'
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
-
-# Below cogs represents our folder our cogs are in. Following is the file name. So 'meme.py' in cogs, would be cogs.meme
-# Think of it like a dot path import
-initial_extensions = ['cogs.game_wow',
-                      'cogs.games',
-                      'cogs.economy',
-                      'cogs.gamble',
-                      'cogs.music',
-                      'cogs.stats',
-                      'cogs.misc',
-                      'cogs.administration',
-                      ]
-
-# BG Task list function
-bg_task_list = [
-    bg_tasks.guild_sub_sending
+initial_extensions = [
+    # 'cogs.games',
+    # 'cogs.economy',
+    # 'cogs.gamble',
+    # 'cogs.stats',
+    'cogs.administration',
+    'cogs.music',
+    'cogs.game_wow',
+    'cogs.misc',
+    # Non-generic cogs
+    'cogs.atb',
 ]
 
-bot_version = '2.4.2'
-bot = commands.Bot(command_prefix=get_prefix, description='Patrisha', formatter=misc.Helper())
-dbm = DBManager()
-me = None
+bot = Patrisha(command_prefix=get_prefix, description='Patrisha', formatter=misc.Helper())
 
-async def background_tasks():
-    await bot.wait_until_ready()
-    print('The bot is now ready!')
-    print('---------------------')
-    while not bot.is_closed():
-        for task in bg_task_list:
-            await task(bot)
-            print(f'Executed BG task: {task.__name__}')
-            # await me.send(f'```<{datetime.datetime.now()}> Executed BG task: {task.__name__}```')
-        await asyncio.sleep(1200)  # task runs every 1200 seconds
-
-# Here we load our extensions(cogs) listed above in [initial_extensions].
 if __name__ == '__main__':
     for extension in initial_extensions:
         try:
+            print(extension)
             bot.load_extension(extension)
         except Exception as e:
             print(f'Failed to load extension {extension}.', file=sys.stderr)
             traceback.print_exc()
 
-
-@bot.event
-async def on_ready():
-    global me
-    await bot.change_presence(activity=discord.Game(name='try using !help'))
-    await bot.user.edit(username=f'Patrisha')
-    me = bot.get_user(217294231125884929)
-
-    print(f'Logged in as: {bot.user.name} - {bot.user.id}')
-    print(f'Version: {bot_version}')
-    print('---------------------')
-
-    for guild in bot.guilds:
-        await dbm.create_settings_if_none(guild)
-        # await guild.get_member(bot.user.id).edit(nick=f'Patrisha')
-        print('>> - Present in', guild)
-    print('---------------------')
-    print(f'Successfully logged in and booted...!')
-    print('---------------------')
-
-
-@bot.event
-async def on_message(message):
-    await bot.process_commands(message)
-
-
-@bot.event
-async def on_guild_join(guild):
-    await dbm.create_settings_if_none(guild)
-
-
-@bot.event
-async def on_member_join(member):
-    server_message = await dbm.get_server_message(member.guild)
-    await member.send(server_message)
-
-    member_role = await dbm.get_server_role(member.guild)
-    for role in member.guild.roles:
-        if member_role.lower() == role.name.lower():
-            await member.add_roles(role, reason='Default role by PatrishaBot')
-
-    await me.send(
-        f'''
-        User {member.name} has joined {member.guild.name}
-        Default role: `{member_role}`
-        Default message: ```{server_message}```
-        '''
-    )
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    print(ctx, error)
-    await me.send(f'{ctx.message.content} caused an error:\n{error}')  # DMs me
-
-    if 'Signature extraction failed' in str(error):
-        await ctx.send('YouTube probably pushed an update that broke something :(')
-    if 'ffmpeg was not found' in str(error):
-        await ctx.send(f'My magnificent creator forgot to give me ffmpeg :)')
-    if isinstance(error, discord.errors.Forbidden):
-        await ctx.send(f'I am missing the required permissions to do this command')
-    if isinstance(error, commands.errors.CommandOnCooldown):
-        # time_str = time.strftime("%H:%M:%S", time.gmtime(int(error.retry_after)))
-        await ctx.send(f'You are on cooldown. Try again in {str(error.retry_after)[:4]}s')
-    if isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.send(f':x: I need more information in order to do that command!\n'
-                       f'Try running `!help <command name>`')
-
-
-bot.loop.create_task(background_tasks())
 bot.run(cfg.BOT_TOKEN, bot=True, reconnect=True)
